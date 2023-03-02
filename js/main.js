fdc099d67258a2b07b15a2873d67366502b370d3
TranDuc41 <td07197@gmail.com>
2023-03-02 11:34:42 +0700

    update template

--- js/main.js
+++ js/main.js
@@ -1,126 +1,129 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Fixed Navbar
-    $(window).scroll(function () {
-        if ($(window).width() < 992) {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow');
-            } else {
-                $('.fixed-top').removeClass('bg-white shadow');
-            }
-        } else {
-            if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-white shadow').css('top', -45);
-            } else {
-                $('.fixed-top').removeClass('bg-white shadow').css('top', 0);
-            }
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+  // Fixed Navbar
+  $(window).scroll(function () {
+    if ($(window).width() < 992) {
+      if ($(this).scrollTop() > 45) {
+        $(".fixed-top").addClass("bg-white shadow");
+      } else {
+        $(".fixed-top").removeClass("bg-white shadow");
+      }
+    } else {
+      if ($(this).scrollTop() > 45) {
+        $(".fixed-top").addClass("bg-white shadow").css("top", -45);
+      } else {
+        $(".fixed-top").removeClass("bg-white shadow").css("top", 0);
+      }
+    }
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        margin: 25,
-        loop: true,
-        center: true,
-        dots: false,
-        nav: true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-
-    
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    margin: 25,
+    loop: true,
+    center: true,
+    dots: false,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 })(jQuery);
 
 // products with carousel slider
-$(document).ready(function()
-{
+$(document).ready(function () {
+  if ($(".bbb_viewed_slider").length) {
+    var viewedSlider = $(".bbb_viewed_slider");
 
-   
-        if($('.bbb_viewed_slider').length)
-        {
-            var viewedSlider = $('.bbb_viewed_slider');
-
-            viewedSlider.owlCarousel(
-            {
-                loop:true,
-                margin:30,
-                autoplay:true,
-                autoplayTimeout:5000,
-                nav:false,
-                dots:false,
-                responsive:
-                {
-                    0:{items:1},
-                    575:{items:2},
-                    768:{items:3},
-                    991:{items:4}
-                }
-            });
-
-            if($('.bbb_viewed_prev').length)
-            {
-                var prev = $('.bbb_viewed_prev');
-                prev.on('click', function()
-                {
-                    viewedSlider.trigger('prev.owl.carousel');
-                });
-            }
-
-            if($('.bbb_viewed_next').length)
-            {
-                var next = $('.bbb_viewed_next');
-                next.on('click', function()
-                {
-                    viewedSlider.trigger('next.owl.carousel');
-                });
-            }
-        }
-
-
+    viewedSlider.owlCarousel({
+      loop: true,
+      margin: 30,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      nav: false,
+      dots: false,
+      responsive: {
+        0: { items: 1 },
+        575: { items: 2 },
+        768: { items: 3 },
+        991: { items: 4 },
+      },
     });
+
+    if ($(".bbb_viewed_prev").length) {
+      var prev = $(".bbb_viewed_prev");
+      prev.on("click", function () {
+        viewedSlider.trigger("prev.owl.carousel");
+      });
+    }
+
+    if ($(".bbb_viewed_next").length) {
+      var next = $(".bbb_viewed_next");
+      next.on("click", function () {
+        viewedSlider.trigger("next.owl.carousel");
+      });
+    }
+  }
+});
+
+//Cart
+
+// log in/ out
+const loginText = document.querySelector(".title-text .login");
+const loginForm = document.querySelector("form.login");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector("form .signup-link a");
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%";
+  loginText.style.marginLeft = "-50%";
+});
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%";
+  loginText.style.marginLeft = "0%";
+});
+signupLink.onclick = (()=>{
+  signupBtn.click();
+  return false;
+});
